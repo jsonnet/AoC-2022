68b7b7f1038064052acfa02fbfe11ac2567b3ff8
Joshua Sonnet <sonnet.joshua@gmail.com>
2022-12-04 10:59:42 +0100

    [DAY04] Challenges solved
    - Easy mode, convert to actual ranges -> check intersection

---
+++ aoc/day04.py
@@ -0,0 +1,17 @@
+with open('day04.txt') as f:
+
+	overlap, partover = 0, 0
+
+	for line in f:
+
+		r = line.strip().split(',')
+		r1, r2 = [int(i) for i in r[0].split("-")], [int(i) for i in r[1].split("-")]
+		r1, r2 = range(r1[0], r1[1]+1), range(r2[0], r2[1]+1)
+
+		if (r1.start in r2 and (r1.stop-1 in r2)) or (r2.start in r1 and (r2.stop-1 in r1)):
+			overlap += 1
+
+		if r1.start in r2 or (r2.start in r1):
+			partover += 1
+
+	print(overlap, partover)
