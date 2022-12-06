68ae556a2c01114a18ddd0cab8c25ef608ae534c
Joshua Sonnet <sonnet.joshua@gmail.com>
2022-12-06 08:03:22 +0100

    [DAY06] Challenges solved
    - Would have liked to use a queue.Queue, but they did not what I expected

---
+++ aoc/day06.py
@@ -0,0 +1,24 @@
+def find_marker(input_str, length) -> int:
+	marker = []
+
+	# start=1 as chars start with counting 1
+	for i, c in enumerate(input_str, start=1):
+		# makeshift FIFO of length length
+		if len(marker) == length:
+			marker.pop(0)
+		marker.append(c)
+
+		# check if marker has only unique chars
+		if len(''.join(set(marker))) == length:
+			print(i, ''.join(set(marker)))
+			return i
+
+
+with open("day06.txt") as f:
+
+	input_str = f.read()
+
+	find_marker(input_str, 4)
+	find_marker(input_str, 14)
+
+	
